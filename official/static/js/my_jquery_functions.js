8e86b2747a81445d7cd7b557516d297ed213f661
zhangshy <zaozh1990@163.com>
2015-01-30 19:41:57 +0800

    测试jQuery滑动

--- official/static/js/my_jquery_functions.js
+++ official/static/js/my_jquery_functions.js
@@ -8,4 +8,18 @@ $(document).ready(function() {
     $(".toggle .btn_toggle").click(function() {
         $(this).parent(".toggle").children(".ex").toggle("slow");
     });
+    $(".flip").click(function() {
+        $(".panel").slideToggle(1000);//1s
+    });
+    $("button.btn_animate").click(function() {
+        var div = $("div.div_animate");
+        div.animate({left:'100px', opacity:'0.4'}, "slow", function() {
+            div.css("color","red");
+        });
+        div.animate({fontSize:'3em'}, "slow", function() {
+            div.css("color","blue");
+        });
+        div.animate({fontSize:'1em'}, "slow");
+        div.animate({left:'0px', opacity:'1'}, "slow");
+    });
 });
